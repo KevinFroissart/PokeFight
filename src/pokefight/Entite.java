09321b7a88cdc246803c26ebbc60fa9747b0ad97
daniont <daniont@meleze08.iut-infobio.priv.univ-lille1.fr>
2019-09-02 16:04:29 +0200

    entité

--- src/pokefight/Entite.java
+++ src/pokefight/Entite.java
@@ -1,9 +1,31 @@
 package pokefight;
 
 public abstract class Entite {
-	String nom;
-	int vie;
-	int degatMin;
-	int degatMax;
-	Type type;
+	private String nom;
+	private int vie;
+	private Type type;
+	
+	public Entite(String nom,int vie,Type type) {
+		this.nom=nom;
+		this.vie=vie;
+		this.type=type;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public int getVie() {
+		return vie;
+	}
+	public void setVie(int vie) {
+		this.vie = vie;
+	}
+	public Type getType() {
+		return type;
+	}
+	
+	
 }
