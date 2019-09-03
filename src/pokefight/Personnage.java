8f1f6ef810cbcdf1acb8d63ad85f65d09125bf42
boulnoih <boulnoih@meleze09.iut-infobio.priv.univ-lille1.fr>
2019-09-03 08:51:03 +0200

    Attaques détaillées

--- src/pokefight/Personnage.java
+++ src/pokefight/Personnage.java
@@ -7,20 +7,50 @@ public class Personnage extends Entite {
 	private int soin;
 	private int dmgVamp;
 	private int healVamp;
+	private String comp1;
+	private String comp2;
+	private String comp3;
 	
 
-	public Personnage(String nom, int vieMax, Type type, int dmgMin, int dmgMax, int soin, int vamp) {
+	public Personnage(String nom, int vieMax, Type type, String comp1, int dmgMin, int dmgMax, String comp2, int soin, String comp3, int vamp) {
 		super(nom, vieMax, type);
 		this.dmgMax = dmgMax;
 		this.dmgMin = dmgMin;
 		this.soin = soin;
 		this.dmgVamp = vamp;
 		this.healVamp = (int) (vamp * 0.5);
+		this.comp1 = comp1;
+		this.comp2 = comp2;
+		this.comp3 = comp3;
 		
 	}
 	
 	public int attaque() {
 		return (int) ((Math.random() * (this.dmgMax - this.dmgMin)) + this.dmgMin);
+	}
+
+	public int getSoin() {
+		return soin;
+	}
+
+	public int getDmgVamp() {
+		return dmgVamp;
+	}
+
+	public int getHealVamp() {
+		return healVamp;
+	}
+
+	public String getComp1() {
+		return comp1;
+	}
+
+	public String getComp2() {
+		return comp2;
+	}
+
+	public String getComp3() {
+		return comp3;
 	}
 	
 }
