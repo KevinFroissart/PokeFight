a9c3bd904a9a2370f1800a436b150ebc2da451ba
daniont <daniont@meleze08.iut-infobio.priv.univ-lille1.fr>
2019-09-03 16:25:09 +0200

    d

--- src/pokefight/ChoixPerso.java
+++ src/pokefight/ChoixPerso.java
@@ -3,41 +3,50 @@ package pokefight;
 import java.util.Scanner;
 
 public class ChoixPerso {
-	
+
 	private static Personnage PersoChoisi;
-	
+
 	public static String scann() {
 		Scanner scan = new Scanner(System.in);
 		return scan.nextLine();
 	}
 
 	public static void main(String[] args) {
-		
+
 		Personnage pikachou = new Personnage("Pikachou", 450, Type.FEU, "Incendie",55, 65, "Foyer",50, "Deplacement thermique",20);
 		Personnage herbisoux = new Personnage("Herbisoux", 500, Type.PLANTE, "Bucheronnage", 40, 50, "Synthese",60, "Vampigrain",35);
 		Personnage magicarpette = new Personnage("Magicarpette", 600, Type.EAU, "Torpille", 35, 55, "Eponge",55, "Trampette",20);		
 		System.out.println("Choisissez votre personnage :");
-		
-		
+
+
 		System.out.println("PokeFighter 1 : Pikachou [FEU]");
 		System.out.println("PokeFighter 2 : Herbisoux [PLANTE]");
 		System.out.println("PokeFighter 3 : Magicarpette [EAU]");
 		System.out.println("Rentrez le numéro de votre PokeFighter");
-		
+
 		boolean ok = false;
-		
+
 		while(!ok) {
-			int scaned = Integer.parseInt(scann());
-			if(scaned > 0 && scaned < 4) ok = true;
-			
-			if(scaned == 1) ChoixPerso.PersoChoisi = pikachou;
 
-			if(scaned == 2) ChoixPerso.PersoChoisi = herbisoux;
+			String scanedT = scann();
 
-			if(scaned == 3) ChoixPerso.PersoChoisi = magicarpette;
-		}	
+			if(scanedT.matches("[1-5]")) {
+				ok = true;
+				int scaned = Integer.parseInt(scann());
+				
+				if(scaned > 0 && scaned < 4) ok = true;
+
+				if(scaned == 1) ChoixPerso.PersoChoisi = pikachou;
+
+				if(scaned == 2) ChoixPerso.PersoChoisi = herbisoux;
+
+				if(scaned == 3) ChoixPerso.PersoChoisi = magicarpette;
+			} else {
+				System.err.println("Entrez un nombre entre 1 et 3 !");
+			}
+		}
 	}
-	
+
 	public static Personnage PersoChoisi() {
 		return ChoixPerso.PersoChoisi;
 	}
