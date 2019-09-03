b83bbdad5ef3c4cb80219424265c81b877ce869b
froissak <froissak@meleze10.iut-infobio.priv.univ-lille1.fr>
2019-09-03 11:23:55 +0200

    score

--- src/pokefight/Score.java
+++ src/pokefight/Score.java
@@ -10,64 +10,82 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Score {
-	
+
 	public static String FileName = "ressources/classement.csv";
-	
-	/*public static void NewScore(String joueur, String score) throws IOException {
-		BufferedReader reader = new BufferedReader(new FileReader(FileName))
-		BufferedReader read = new BufferedReader(new FileReader(FileName));
-		List<String[]> list = new ArrayList<>();
-        String line = "";
-        while((line = reader.readLine()) != null){
-            String[] array = line.split(separator);
-            list.add(array);
-        }
-		BufferedWriter classement = new BufferedWriter(new FileWriter(FileName));
-		int compteur = 0;
-		while (read.readLine() != null) {
-			 compteur++;
-		}
-		read.close();
 
-		classement.append(joueur);
-		classement.append(",");
-		classement.append(score);
-		classement.append("\n");
-		
-		classement.flush();
-		classement.close();
 
+	public static List<String[]> trie(List<String[]> list) {
+		int longueur = list.size();
+		String tampon0 = "";
+		String tampon1 = "";
+		boolean permut;
+
+		do {
+			permut = false;
+			for (int i = 0; i < longueur - 1; i++) {
+				if (Integer.parseInt(list.get(i)[1]) < Integer.parseInt(list.get(i+1)[1])) {
+					tampon0 = list.get(i)[0];
+					tampon1 = list.get(i)[1];
+					list.get(i)[1] = list.get(i+1)[1];
+					list.get(i)[0] = list.get(i+1)[0];
+					list.get(i+1)[0] = tampon0;
+					list.get(i+1)[1] = tampon1;
+					permut = true;
+				}
+			}
+		} 
+		while (permut);
+		return list;
 	}
-	*/
+
+	public static void NewScore(String joueur, String score) throws IOException {
+		try
+		{
+		 FileWriter fw = new FileWriter(FileName,true);
+		 fw.write(joueur+","+score+"\n");
+		 fw.close();
+		}
+		catch(IOException ioe)
+		{
+		 System.err.println("IOException:" + ioe.getMessage());
+		}
+	}
+
 	public static List<String[]> Affichage(String separator) {
-	    try (BufferedReader reader = new BufferedReader(new FileReader(FileName))){
-	        List<String[]> list = new ArrayList<>();
-	        String line = "";
-	        while((line = reader.readLine()) != null){
-	            String[] array = line.split(separator);
-	            list.add(array);
-	        }
-	        return list;
-	    } catch (IOException e) {
-	        e.printStackTrace();
-	        return null;
-	    }  
+		try (BufferedReader reader = new BufferedReader(new FileReader(FileName))){
+			List<String[]> list = new ArrayList<>();
+			String line = "";
+			while((line = reader.readLine()) != null){
+				String[] array = line.split(separator);
+				list.add(array);
+			}
+			return list;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}  
 	}
 
 	public static void main(String args[]) throws IOException {
 
 		BufferedReader read;
-		//NewScore("Test1", "40");
+		//NewScore("Test1", "10");
+		//NewScore("Test2", "40");
+		NewScore("Test3", "50");
+		//NewScore("Test4", "20");
+		//NewScore("Test5", "140");
 
 		try {
 			read = new BufferedReader(new FileReader(FileName));
 			List<String[]> list = Affichage(",");
-			 for(int i=0;i<list.size();i++){
-				    System.out.print(list.get(i)[0]);
-				    System.out.print(" : ");
-				    System.out.print(list.get(i)[1]);
-				    System.out.println(" points");
-				} 			
+			list = trie(list);
+			for(int i=0;i<list.size();i++){
+				System.out.print(list.get(i)[0]);
+				System.out.print(" : ");
+				System.out.print(list.get(i)[1]);
+				System.out.println(" points");
+			} 	
+			read.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}	
